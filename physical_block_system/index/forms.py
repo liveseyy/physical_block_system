495d1d099629cdc9a952b4185f22b8c7e18ba2d2
Dmitry Ermilov <dmitry.ermilov@fabrique.studio>
2022-03-19 12:08:44 +0300

    Добавлены итоговые параметры для расчётов

--- physical_block_system/index/forms.py
+++ physical_block_system/index/forms.py
@@ -4,7 +4,9 @@ from django import forms
 class PhysicalObjectiveForm(forms.Form):
     m1 = forms.IntegerField(label='m₁', required=False)
     m2 = forms.IntegerField(label='m₂', required=False)
-    a1 = forms.IntegerField(label='a₁', required=False)
-    a2 = forms.IntegerField(label='a₂', required=False)
-
     m = forms.IntegerField(label='m', required=False)
+
+    T1 = forms.IntegerField(label='T₁', required=False)
+    T2 = forms.IntegerField(label='T₂', required=False)
+
+    S_table = forms.IntegerField(label='Длина стола', required=False)
